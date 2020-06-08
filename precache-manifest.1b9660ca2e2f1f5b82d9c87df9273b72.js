8806b8ea7ce1cbcd36d77e19824a0193f683c960
jatin0814 <201951073@iiitvadodara.ac.in>
2020-06-08 20:49:53 +0530

    Updates

---
+++ precache-manifest.1b9660ca2e2f1f5b82d9c87df9273b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9e561527e438baa9beec11f7650a4e",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "15ea752684dedbc161c9",
+    "url": "/Netflix/static/css/main.6bff5563.chunk.css"
+  },
+  {
+    "revision": "6a2b9073905ff8449446",
+    "url": "/Netflix/static/js/2.2328e6b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Netflix/static/js/2.2328e6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ea752684dedbc161c9",
+    "url": "/Netflix/static/js/main.762d3b5e.chunk.js"
+  },
+  {
+    "revision": "3119bdd26bd5c8b5b3ca",
+    "url": "/Netflix/static/js/runtime-main.0c31e96b.js"
+  },
+  {
+    "revision": "763484ffec2946f2d53aa7bd932c59ac",
+    "url": "/Netflix/static/media/main.763484ff.jpg"
+  }
+]);
